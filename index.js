992737730e1f25a141c776deb2071f842ffa3166
MyDBot <40378113+MyDBot@users.noreply.github.com>
2018-06-19 07:54:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -88,6 +88,18 @@ bot.login(botconfig.token);
   
   
   
+var express = require("express");
+var app= express();
+var port = process.env.PORT || 80;
+
+app.get("/" ,function(req , res){
+	res.send("welcome to F MyD Demo!");
+});
+
+app.get('/tst' , (req, res) => res.send("tst Page"))
+
+
+app.listen(port);
   
 var http = require("http");
 setInterval(function() {
