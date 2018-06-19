61d5d9fe8c76f4bd93a1f8048f247009944907fb
MyDBot <40378113+MyDBot@users.noreply.github.com>
2018-06-19 07:37:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -84,3 +84,20 @@ bot.on("message" , async message=>{
   }
 });
 bot.login(botconfig.token);
+
+  
+  
+  
+  
+  var express = require("express");
+var app= express();
+var port = process.env.PORT || 2000;
+
+app.get("/" ,function(req , res){
+	res.send("welcome to F MyD Demo!");
+});
+
+app.get('/tst' , (req, res) => res.send("tst Page"))
+
+
+app.listen(port);
