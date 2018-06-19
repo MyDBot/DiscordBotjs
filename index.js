128ac62951bbb7b151c5dac73d055b2ecf6ec7fb
MyDBot <40378113+MyDBot@users.noreply.github.com>
2018-06-19 09:38:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ const bot = new Discord.Client({disableEveryone: true});
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 
-fs.readdir("./commands/" , (err,files)=>{
+
+fs.readdir("./commands/", (err, files) => {
 
   if(err) console.log(err);
-  let jsfile = files.filter(f=>f.split(".").pop() === "js")
-  if(jsfile.length<=0){
-    console.log("Couldnt find Commands!");
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
     return;
   }
-  jsfile.forEach((f,i)=>{
+
+  jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
-    console.log(`${f} Loaded!`);
-    bot.commands.set(props.help.name , props);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
   });
 
 });
+
 
 bot.on("ready" , async () => {
   console.log(`${bot.user.username} is online!`);
