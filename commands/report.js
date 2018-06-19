f34b5b141764cece8585a3373f113d1dd04fcfa9
MyDBot <40378113+MyDBot@users.noreply.github.com>
2018-06-19 09:46:57 +0300

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,6 +1,17 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message , args)=>{
-  
+   let ruser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+     if(!ruser) return message.channel.send("Couldn`t Find User!");
+     let reason = args.join().slice(22);
+   
+     let rEmbed = new Discord.RichEmbed()
+     .setDescription("Report")
+     .setColor("#00ff61")
+     .addField("Reported User: " , `${ruser}`)
+     .addField("Reason: " , `${reason}`)
+     .addField("Status: ","sent!");
+   
+     return message.channel.send(rEmbed);
 }
 
 module.exports.help={
