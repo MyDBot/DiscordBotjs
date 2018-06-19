f3365d99549b3ed4bcdb203d3e36a8c0e1d9f4c1
MyDBot <40378113+MyDBot@users.noreply.github.com>
2018-06-19 11:15:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,3 +50,9 @@ setInterval(function() {
 }, 10000);
 
 
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080); 
